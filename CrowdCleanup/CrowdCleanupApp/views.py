4e2bafb61c051b67d93130f8dbff4d3e99a64072
Brock Milaszewski <bmilaszewski@clarku.edu>
2024-02-24 17:23:09 -0500

    pins

--- CrowdCleanup/CrowdCleanupApp/views.py
+++ CrowdCleanup/CrowdCleanupApp/views.py
@@ -7,13 +7,27 @@ def home(request):
 
 def report_litter(request):
     if request.method == 'POST':
+        description = request.POST.get('description', '')
+        latitude = request.POST.get('latitude', '')
+        longitude = request.POST.get('longitude', '')
+        zip_code = request.POST.get('zip_code', '')
+
+        # Print the data in the command prompt
+        print(f"Description: {description}")
+        print(f"Latitude: {latitude}")
+        print(f"Longitude: {longitude}")
+        print(f"Zip Code: {zip_code}")
         # Assuming you handle form submission here
         # Redirect to afterSubmit page after form submission
         return redirect('after_submit')  # Redirects to the URL with name 'after_submit'
     return render(request, 'report_litter.html')
 
 def clean_litter(request):
-    # Logic for cleaning up litter
+    zip_code = request.GET.get('zip_code', None)
+    
+    # Use the zip code as needed in your view logic
+    
+    # Render the clean_litter template with any context data
     return render(request, 'clean_litter.html')  # Assuming you have a template named clean_litter.html
 
 def pin_view(request, pin_id):
