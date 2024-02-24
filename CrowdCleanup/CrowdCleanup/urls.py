39ad908095889b1ac908aa5f3e45ce6e48a263a9
Brock Milaszewski <bmilaszewski@clarku.edu>
2024-02-23 21:55:10 -0500

    home

--- CrowdCleanup/CrowdCleanup/urls.py
+++ CrowdCleanup/CrowdCleanup/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     # Define URLs for reporting litter and cleaning litter views
-    # path('report_litter/', views.report_litter, name='report_litter'),
-    # path('clean_litter/', views.clean_litter, name='clean_litter'),
+    path('report_litter/', views.report_litter, name='report_litter'),
+    path('clean_litter/', views.clean_litter, name='clean_litter'),
 ]
