1c62a2d612ca49cc8bfc2f1cb80c872fb8f98632
ChristopherSiems <siemsck@gmail.com>
2024-02-24 03:56:03 -0500

    change

--- CrowdCleanup/CrowdCleanupApp/models.py
+++ CrowdCleanup/CrowdCleanupApp/models.py
@@ -6,9 +6,5 @@ class Pin(models.Model):
   date = models.DateTimeField(auto_now_add=True)
   description = models.TextField()
   image = models.ImageField(upload_to='media/', null=True, blank=True)
-<<<<<<< Updated upstream:CrowdCleanup/CrowdCleanupApp/models.py
-  status = models.CharField(choices=STATUS_OPTIONS, default="to_be_cleaned", max_length = 13)
-=======
   status = models.CharField( default='TO BE CLEANED', max_length = 13)
->>>>>>> Stashed changes:CrowdCleanup/CrowdCleanup/models.py
   description = models.TextField()
