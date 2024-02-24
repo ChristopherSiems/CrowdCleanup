217dff5a27d863e6c7a36e06324e38e5f7f96034
ChristopherSiems <siemsck@gmail.com>
2024-02-24 03:52:30 -0500

    changes

--- CrowdCleanup/CrowdCleanup/settings.py
+++ CrowdCleanup/CrowdCleanup/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
--- CrowdCleanup/CrowdCleanup/urls.py
+++ CrowdCleanup/CrowdCleanup/urls.py
@@ -1,12 +1,25 @@
 # urls.py
 from django.urls import path
 from django.contrib import admin
+<<<<<<< Updated upstream
 from CrowdCleanupApp import views
+=======
+from django.conf import settings
+from django.conf.urls.static import static
+from CrowdCleanupApp import views
+
+>>>>>>> Stashed changes
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('report_litter/', views.report_litter, name='report_litter'),
     path('clean_litter/', views.clean_litter, name='clean_litter'),
+<<<<<<< Updated upstream
     path('pin_view/', views.pin_view, name = 'pin_view'),
     path("admin/", admin.site.urls),
 ]
+=======
+    path('pin_view/<int:pin_id>/', views.pin_view, name = 'pin_view'),
+    path("admin/", admin.site.urls),
+]
+>>>>>>> Stashed changes
